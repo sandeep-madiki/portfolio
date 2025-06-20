3d198c7dcd05e42bc6dfe3c91696e927025a7a96
Sandeep Madiki <sandeep@kloudworx.com>
2025-06-20 11:41:13 +0530

    skill card adjustment

--- src/Component/Skills/Skills.tsx
+++ src/Component/Skills/Skills.tsx
@@ -6,21 +6,21 @@ import HTMLIcon from "../../assets/Tech-icons/HTML5.png";
 import CSSIcon from "../../assets/Tech-icons/CSS3.png";
 import TaiwindIcon from "../../assets/Tech-icons/Tailwind CSS.png";
 import BootstrapIcon from "../../assets/Tech-icons/Bootstrap.png";
-import SCSSIcon from "../../assets/Tech-icons/sass.svg";
+// import SCSSIcon from "../../assets/Tech-icons/sass.svg";
 import JSIcon from "../../assets/Tech-icons/JavaScript.png";
 import TSIcon from "../../assets/Tech-icons/TypeScript.png";
 import NodeIcon from "../../assets/Tech-icons/Node.js.png";
 import ExpressIcon from "../../assets/Tech-icons/Express.png";
 import MongoDBIcon from "../../assets/Tech-icons/MongoDB.png";
-import MongooseIcon from "../../assets/Tech-icons/Mongoose.js.png";
+// import MongooseIcon from "../../assets/Tech-icons/Mongoose.js.png";
 import SqlIcon from "../../assets/Tech-icons/SQLite.png";
 import GitIcon from "../../assets/Tech-icons/Git.png";
 import VSCodeIcon from "../../assets/Tech-icons/Visual Studio Code (VS Code).png";
 import PostmanIcon from "../../assets/Tech-icons/Postman.png";
 import VercelIcon from "../../assets/Tech-icons/Vercel.png";
-import NetlifyIcon from "../../assets/Tech-icons/Netlify.webp";
-import DockerIcon from "../../assets/Tech-icons/Docker.png";
-import FigmaIcon from "../../assets/Tech-icons/Figma.png";
+// import NetlifyIcon from "../../assets/Tech-icons/Netlify.webp";
+// import DockerIcon from "../../assets/Tech-icons/Docker.png";
+// import FigmaIcon from "../../assets/Tech-icons/Figma.png";
 import JSONIcon from "../../assets/Tech-icons/JSON.png";
 
 const skills1 = [
@@ -31,21 +31,21 @@ const skills1 = [
   { name: "CSS", icon: CSSIcon },
   { name: "Tailwind CSS", icon: TaiwindIcon },
   { name: "Bootstrap", icon: BootstrapIcon },
-  { name: "SCSS", icon: SCSSIcon },
+  // { name: "SCSS", icon: SCSSIcon },
   { name: "JavaScript", icon: JSIcon },
   { name: "TypeScript", icon: TSIcon },
   { name: "Node JS", icon: NodeIcon },
   { name: "Express JS", icon: ExpressIcon },
   { name: "MongoDB", icon: MongoDBIcon },
-  { name: "Mongoose", icon: MongooseIcon },
+  // { name: "Mongoose", icon: MongooseIcon },
   { name: "SQL", icon: SqlIcon },
   { name: "Git", icon: GitIcon },
   { name: "VSCode", icon: VSCodeIcon },
   { name: "Postman", icon: PostmanIcon },
   { name: "Vercel", icon: VercelIcon },
-  { name: "Netlify", icon: NetlifyIcon },
-  { name: "Docker (Basics)", icon: DockerIcon },
-  { name: "Figma", icon: FigmaIcon },
+  // { name: "Netlify", icon: NetlifyIcon },
+  // { name: "Docker (Basics)", icon: DockerIcon },
+  // { name: "Figma", icon: FigmaIcon },
   { name: "JSON", icon: JSONIcon },
 ];
 
