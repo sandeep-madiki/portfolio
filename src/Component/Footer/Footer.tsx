eb4ac2e9a4d3a2c9c039948425881e367bf88ccd
Sandeep Madiki <sandeep@kloudworx.com>
2025-08-26 10:48:44 +0530

    branch commit

--- src/Component/Footer/Footer.tsx
+++ src/Component/Footer/Footer.tsx
@@ -155,7 +155,7 @@ const Footer = () => {
           </div>
         </div>
         <div className="footer-bottom">
-          {/* <p>&copy; {currentYear} Sandeep Madiki. All rights reserved.</p> */}
+          <p>&copy; {currentYear} Sandeep Madiki. All rights reserved.</p>
           <p>&copy; {currentYear} Sandeep Madiki.</p>
           <p className="footer-location">Based in AP, India</p>
         </div>
